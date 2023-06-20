d285a10c026295ba2f48c24aebf58060bbd8efcf
Philip Cesar Garay <philipgaray2@gmail.com>
2023-06-21 01:13:34 +0800

    Update Fonts using expo-fonts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
 
 import 'react-native-gesture-handler';
@@ -8,9 +8,34 @@ import RootNavigators from './navigations';
 
 import * as SplashScreen from 'expo-splash-screen';
 
+import {
+  useFonts,
+  Montserrat_100Thin,
+  Montserrat_200ExtraLight,
+  Montserrat_300Light,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+} from '@expo-google-fonts/montserrat';
+
 SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_100Thin,
+    Montserrat_200ExtraLight,
+    Montserrat_300Light,
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+    Montserrat_800ExtraBold,
+    Montserrat_900Black,
+  });
+
   useEffect(() => {
     async function prepare() {
       await SplashScreen.hideAsync();
@@ -18,6 +43,10 @@ const App = () => {
 
     prepare();
   }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <SafeAreaProvider>
--- src/constants/Typography.js
+++ src/constants/Typography.js
@@ -7,29 +7,41 @@ export const FONT_WEIGHT_REGULAR = '400';
 export const FONT_WEIGHT_MEDIUM = '500';
 export const FONT_WEIGHT_SEMI_BOLD = '600';
 export const FONT_WEIGHT_BOLD = '700';
+export const FONT_WEIGHT_EXTRA_BOLD = '800';
+export const FONT_WEIGHT_BLACK = '900';
 
 // FONT STYLE
 export const FONT_LIGHT = {
-  fontFamily: FONT_FAMILY,
+  fontFamily: 'Montserrat_300Light',
   fontWeight: FONT_WEIGHT_LIGHT,
 };
 
 export const FONT_REGULAR = {
-  fontFamily: FONT_FAMILY,
+  fontFamily: 'Montserrat_400Regular',
   fontWeight: FONT_WEIGHT_REGULAR,
 };
 
 export const FONT_MEDIUM = {
-  fontFamily: FONT_FAMILY,
+  fontFamily: 'Montserrat_500Medium',
   fontWeight: FONT_WEIGHT_MEDIUM,
 };
 
 export const FONT_SEMI_BOLD = {
-  fontFamily: FONT_FAMILY,
+  fontFamily: 'Montserrat_600SemiBold',
   fontWeight: FONT_WEIGHT_SEMI_BOLD,
 };
 
 export const FONT_BOLD = {
-  fontFamily: FONT_FAMILY,
+  fontFamily: 'Montserrat_700Bold',
   fontWeight: FONT_WEIGHT_BOLD,
+};
+
+export const FONT_EXTRA_BOLD = {
+  fontFamily: 'Montserrat_800ExtraBold',
+  fontWeight: FONT_WEIGHT_EXTRA_BOLD,
+};
+
+export const FONT_BLACK = {
+  fontFamily: 'Montserrat_900Black',
+  fontWeight: FONT_WEIGHT_BLACK,
 };
